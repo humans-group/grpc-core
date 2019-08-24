6d41687bbf4ee1814e4edf4e74046a15321e5a61
Maxim Korolyov <korolyov.maxim@gmail.com>
2019-08-24 23:27:14 +0300

    code cleanup

--- discovery/consul/resolver.go
+++ discovery/consul/resolver.go
@@ -1,23 +1,12 @@
 package consul
 
 import (
-	"errors"
 	"fmt"
+	"sync"
+
 	"github.com/hashicorp/consul/api"
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/resolver"
-	"regexp"
-	"sync"
-)
-
-const (
-	defaultPort = "8500"
-)
-
-var (
-	errMissingAddr  = errors.New("consul resolver: missing address")
-	errAddrMisMatch = errors.New("consul resolver: invalid uri")
-	regexConsul     = regexp.MustCompile("^([A-z0-9.]+)(:[0-9]{1,5})?/([A-z_]+)$")
 )
 
 func Init() {
@@ -64,13 +53,12 @@ func (cr *consulResolver) watcher() {
 	}
 
 	for {
-		grpclog.Infof("consul resolver %+v", *cr)
-		services, metainfo, err := client.Health().Service(cr.name, "", true, &api.QueryOptions{WaitIndex: cr.lastIndex})
+		services, metainfo, err := client.Health().Service(cr.name, env, true,
+			&api.QueryOptions{WaitIndex: cr.lastIndex})
 		if err != nil {
 			grpclog.Errorf("error retrieving instances from Consul: %v", err)
 			continue
 		}
-		grpclog.Infof("consul resolver got services %+v and meta %+v", services, metainfo)
 
 		cr.lastIndex = metainfo.LastIndex
 		var newAddrs []resolver.Address
@@ -78,7 +66,9 @@ func (cr *consulResolver) watcher() {
 			addr := fmt.Sprintf("%v:%v", service.Service.Address, service.Service.Port)
 			newAddrs = append(newAddrs, resolver.Address{Addr: addr})
 		}
-		grpclog.Infof("consul resolved discovered new addresses %+v", newAddrs)
+
+		grpclog.Infof("consul resolver got new addresses %+v", newAddrs)
+
 		cr.cc.UpdateState(resolver.State{
 			Addresses: newAddrs,
 		})
