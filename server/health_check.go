05fb4bb27a8ae8bce3c1b7ff192ab05972464421
Maxim Korolyov <korolyov.maxim@gmail.com>
2019-08-24 13:43:56 +0300

    health check fix

--- server/health_check.go
+++ server/health_check.go
@@ -3,12 +3,15 @@ package server
 import (
 	"context"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
+	"google.golang.org/grpc/grpclog"
 
 	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
 //health
-type HealthCheck struct{}
+type HealthCheck struct {
+	watchServer grpc_health_v1.Health_WatchServer
+}
 
 var healthCheckResponse = &grpc_health_v1.HealthCheckResponse{
 	Status: grpc_health_v1.HealthCheckResponse_SERVING,
@@ -22,5 +25,20 @@ func (h *HealthCheck) Check(ctx context.Context, req *grpc_health_v1.HealthCheck
 }
 
 func (h *HealthCheck) Watch(req *grpc_health_v1.HealthCheckRequest, w grpc_health_v1.Health_WatchServer) error {
+	grpclog.Infof("received health check from watching service %+v", req)
+	if err := w.Send(healthCheckResponse); err != nil {
+		grpclog.Errorf("failed to send health check watch respose: %v", err)
+		return err
+	}
+	return nil
+}
+
+func (h *HealthCheck) shutdown() error {
+	if err := h.watchServer.Send(&grpc_health_v1.HealthCheckResponse{Status: grpc_health_v1.
+		HealthCheckResponse_NOT_SERVING}); err != nil {
+		grpclog.Errorf("failed to send NOT_SERVING to Health Check Watcher")
+		return err
+	}
+
 	return nil
 }
